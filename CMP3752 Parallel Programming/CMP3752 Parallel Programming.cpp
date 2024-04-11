4d98aa8faca3799f39ccc673ef7e85090049bb25
Aqpogthy <102671423+Aqpogthy@users.noreply.github.com>
2024-04-11 03:44:38 +0100

    fini
    just need to delete tutorials and add final polish

--- CMP3752 Parallel Programming/CMP3752 Parallel Programming.cpp
+++ CMP3752 Parallel Programming/CMP3752 Parallel Programming.cpp
@@ -37,12 +37,7 @@ int main(int argc, char** argv) {
 		CImg<unsigned char> image_input(image_filename.c_str());
 		CImgDisplay disp_input(image_input, "input");
 
-		//a 3x3 convolution mask implementing an averaging filter
-		std::vector<float> convolution_mask = { 1.f / 9, 1.f / 9, 1.f / 9,
-												1.f / 9, 1.f / 9, 1.f / 9,
-												1.f / 9, 1.f / 9, 1.f / 9 };
-
-		//Part 3 - host operations
+		//host operations
 		//3.1 Select computing devices
 		cl::Context context = GetContext(platform_id, device_id);
 
@@ -50,9 +45,9 @@ int main(int argc, char** argv) {
 		std::cout << "Running on " << GetPlatformName(platform_id) << ", " << GetDeviceName(platform_id, device_id) << std::endl;
 
 		//create a queue to which we will push commands for the device
-		cl::CommandQueue queue(context);
+		cl::CommandQueue queue(context, CL_QUEUE_PROFILING_ENABLE);
 
-		//3.2 Load & build the device code
+		// Load & build the device code
 		cl::Program::Sources sources;
 
 		AddSources(sources, "kernels.cl");
@@ -70,15 +65,16 @@ int main(int argc, char** argv) {
 			throw err;
 		}
 
-
-		//Part 4 - device operations
-
-		//device - buffers
 		typedef int mytype;
 
 		float nr_bins = 256;
 
-		std::vector<mytype> B(256);
+		//int local_size = 8;
+
+		std::vector<mytype> B(nr_bins);
+
+		//device - buffers
+
 		size_t histogram_size = nr_bins * sizeof(mytype);
 
 		cl::Buffer Histogram(context, CL_MEM_READ_WRITE, histogram_size);
@@ -89,24 +85,33 @@ int main(int argc, char** argv) {
 
 		cl::Buffer NormalisedHistogram(context, CL_MEM_READ_WRITE, histogram_size);
 		
-		cl::Buffer dev_image_output(context, CL_MEM_READ_WRITE, image_input.size()); //should be the same as input image
+		cl::Buffer dev_image_output(context, CL_MEM_READ_WRITE, image_input.size());
 		
-		float SF = nr_bins / image_input.size();
+		float SF = nr_bins / image_input.size(); //sets the scale factor which will be used to normalise and scale the cumulative histogram
 
 		cl::Buffer ScaleFactor(context, CL_MEM_READ_ONLY, sizeof(float));
 
-				//4.1 Copy images to device memory
-		queue.enqueueWriteBuffer(dev_image_input, CL_TRUE, 0, image_input.size(), &image_input.data()[0]);
+		
+		queue.enqueueWriteBuffer(dev_image_input, CL_TRUE, 0, image_input.size(), &image_input.data()[0]);//Copy images to device memory
 		queue.enqueueFillBuffer(Histogram, 0, 0, histogram_size);
 		queue.enqueueFillBuffer(CumulativeHistogram, 0, 0, histogram_size);
 		queue.enqueueFillBuffer(NormalisedHistogram, 0, 0, histogram_size);
-		queue.enqueueWriteBuffer(ScaleFactor, CL_TRUE, 0, sizeof(float), &SF);
 
 
-				//4.2 Setup and execute the kernel (i.e. device code)
+		//Setup and execute the kernel (i.e. device code)
 		cl::Kernel HistogramKernel = cl::Kernel(program, "intensityHistogram");
 		HistogramKernel.setArg(0, dev_image_input);
 		HistogramKernel.setArg(1, Histogram);
+
+		cl::Kernel LocalHistogramKernel = cl::Kernel(program, "intensityHistogramLocal");
+		cl::Device device = context.getInfo<CL_CONTEXT_DEVICES>()[0]; // get device
+		//std::cout << LocalHistogramKernel.getWorkGroupInfo<CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE>	(device) << endl; // get info
+		size_t local_size = LocalHistogramKernel.getWorkGroupInfo<CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE>(device);
+		LocalHistogramKernel.setArg(0, dev_image_input);
+		LocalHistogramKernel.setArg(1, cl::Local(local_size * sizeof(mytype) * nr_bins));
+		LocalHistogramKernel.setArg(2, Histogram);
+		LocalHistogramKernel.setArg(3, nr_bins);
+
 
 		cl::Kernel CumulativeHistogramKernel = cl::Kernel(program, "cumulativeHistogram");
 		CumulativeHistogramKernel.setArg(0, Histogram);
@@ -121,25 +126,47 @@ int main(int argc, char** argv) {
 		BackProjectionKernel.setArg(0, dev_image_input);
 		BackProjectionKernel.setArg(1, NormalisedHistogram);
 		BackProjectionKernel.setArg(2, dev_image_output);
-		//Normalise.setArg(3, nr_bins);
-		//kernel.setArg(1, dev_image_output);
-		//kernel.setArg(2, dev_convolution_mask);
-		
-		//queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(image_input.width(), image_input.height(), image_input.spectrum()), cl::NullRange);
-		queue.enqueueNDRangeKernel(HistogramKernel, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange);
-		queue.enqueueNDRangeKernel(CumulativeHistogramKernel, cl::NullRange, cl::NDRange(histogram_size), cl::NullRange);
-		queue.enqueueNDRangeKernel(NormaliseKernel, cl::NullRange, cl::NDRange(histogram_size), cl::NullRange);
-		queue.enqueueNDRangeKernel(BackProjectionKernel, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange);
-		std::cout << "hello" << endl;
 
+		cl::Event prof_event1;
+		cl::Event prof_event2;
+		cl::Event prof_event3;
+		cl::Event prof_event4;
+
+		//queue.enqueueNDRangeKernel(HistogramKernel, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange, NULL, &prof_event1);
+		queue.enqueueNDRangeKernel(LocalHistogramKernel, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange, NULL, &prof_event1);//just comment this out and uncomment the one above out to swap to global mem histogram kernel
+		queue.enqueueNDRangeKernel(CumulativeHistogramKernel, cl::NullRange, cl::NDRange(histogram_size), cl::NullRange, NULL, &prof_event2);
+		queue.enqueueNDRangeKernel(NormaliseKernel, cl::NullRange, cl::NDRange(histogram_size), cl::NullRange, NULL, &prof_event3);
+		queue.enqueueNDRangeKernel(BackProjectionKernel, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange, NULL, &prof_event4);
+		
+		//Copy the result from device to host and display
 		vector<unsigned char> output_buffer(image_input.size());
-		////4.3 Copy the result from device to host
 		queue.enqueueReadBuffer(dev_image_output, CL_TRUE, 0, image_input.size(), &output_buffer.data()[0]);
-		std::cout << "hello" << endl;
-		//std::cout << "B = " << B << std::endl;
-		std::cout << "hello" << endl;
 		CImg<unsigned char> output_image(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
 		CImgDisplay disp_output(output_image, "output");
+
+		typedef int mytype2;
+		cout << endl;
+		std::vector<mytype2> H((int)nr_bins);
+		queue.enqueueReadBuffer(Histogram, CL_TRUE, 0, histogram_size, &H[0]);
+		std::cout << "Histogram = " << H << std::endl;
+		std::cout << "Histogram kernel execution time [ns]: " << prof_event1.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_event1.getProfilingInfo<CL_PROFILING_COMMAND_START>() << std::endl;
+		std::cout << GetFullProfilingInfo(prof_event1, ProfilingResolution::PROF_US) << endl;
+		cout << endl;
+
+		std::vector<mytype2> CH((int)nr_bins);
+		queue.enqueueReadBuffer(CumulativeHistogram, CL_TRUE, 0, histogram_size, &CH[0]);
+		std::cout << "Cumulative Histogram = " << CH << std::endl;
+		std::cout << "Cumulative Histogram kernel execution time [ns]: " << prof_event2.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_event2.getProfilingInfo<CL_PROFILING_COMMAND_START>() << std::endl;
+		std::cout << GetFullProfilingInfo(prof_event2, ProfilingResolution::PROF_US) << endl;
+		cout << endl;
+
+		std::vector<mytype2> NH((int)nr_bins);
+		queue.enqueueReadBuffer(NormalisedHistogram, CL_TRUE, 0, histogram_size, &NH[0]);
+		std::cout << "NH = " << NH << std::endl;
+		std::cout << "Normalised Histogram kernel execution time [ns]: " << prof_event3.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_event3.getProfilingInfo<CL_PROFILING_COMMAND_START>() << std::endl;
+		std::cout << GetFullProfilingInfo(prof_event3, ProfilingResolution::PROF_US) << endl;
+		cout << endl;
+
 		while (!disp_input.is_closed() && !disp_output.is_closed()
 			&& !disp_input.is_keyESC() && !disp_output.is_keyESC()) {
 			disp_input.wait(1);
--- CMP3752 Parallel Programming/kernels/kernels.cl
+++ CMP3752 Parallel Programming/kernels/kernels.cl
@@ -1,166 +1,32 @@
-﻿//a simple OpenCL kernel which copies all pixels from A to B
-kernel void identity(global const uchar* A, global uchar* B) {
-	int id = get_global_id(0);
-	B[id] = A[id];
-}
-
-kernel void filter_r(global const uchar* A, global uchar* B) {
-	int id = get_global_id(0);
-	int image_size = get_global_size(0) / 3; //each image consists of 3 colour channels
-	int colour_channel = id / image_size; // 0 - red, 1 - green, 2 - blue
-	//this is just a copy operation, modify to filter out the individual colour channels
-
-	if (colour_channel == 0) {
-		B[id] = A[id];
-	}
-	else {
-		B[id] = 0;
-	}
-
-}
-kernel void invert(global const uchar* A, global uchar* B) {
-	int id = get_global_id(0);
-	B[id] = 255 - A[id];
-}
-//        ░░         ░ ░    ░░░░░░ ░       ░░░░          ░░░      
-//        ▒█ ░      ░░█░░  ░█░░ ▒███░░ ░   ██            ░█       
-//        █░          ░██ ░ ██ ░   ░▒█▒░  ▓█              █       
-//       ░█             ░█░ ░█▒    ░ ░ ██▒█               █       
-//       ░█              ░██████░       ░█▒░         ░    █       
-//       ░█             ░██▒░░            ░               █       
-//       ░█              ░▒▒▒▓░ ░                         █       
-//       ░█                                               █░      
-//       ░█                                              ▒█       
-//        █                                             ░█        
-//       ░██         ░░░░░  ░     ░░  ░▒▒█████████      ██        
-//         █░░   ░██░ ░██████        ░██████   █▒     ░██         
-//          █░░  ░█░  ░██████░        ██████   ░█  ░░██▓░░░       
-//    ░██████▓▒░ █▒   ░██████        ░██████░   █  ░░  ░ ▒█░      
-//     ░█▓    ░  █     █████▒         █████▒    █ ░    ░█▒ ░      
-//      ░░█▓     ▓█   ░░███░  ░░      ░░░░    ░ █ ░ ░░██          
-//          █▒░▒█▓         ░   ░░          ░░▒█░▓█░  ▓█           
-//         ██ ░░▓██▓░      ░          ▒░    ░░██▒░░░  ░█          
-//        ▒█               ▒▓░▒█▒▓████▒░      ░░░ ░░   ░█░        
-//        █▒ ░░▒█░░       ░   ░              ░ ░██▒▒▒████▒        
-//             ░░ ███▒░░░             ░ ░ ▒████░                  
-//                   ████████            ░ ▒                      
-//                  ░░█                    ░█░                    
-//                     ▒██                  ░█                    
-//                    ░▓█░                   ▒█                   
-//                    █▒░░░░                 ░▒█                  
-//                      ░█▓                  ░░█░                 
-//                      ▒█                     ▓█                 
-//                      █░                     ░█░                
-//							 Vesper's Work
-kernel void rgb2grey(global const uchar* A, global uchar* B) {
-	int id = get_global_id(0);
-	int image_size = get_global_size(0) / 3; //each image consists of 3 colour channels
-	int colour_channel = id / image_size; // 0 - red, 1 - green, 2 - blue
-
-	int red = 255;
-	int green = 255;
-	int blue = 255;
-
-	if (colour_channel == 0) {
-		red = A[id];
-		green = A[id + image_size];
-		blue = A[id + image_size + image_size];
-	}
-	if (colour_channel == 1) {
-		red = A[id - image_size];
-		green = A[id];
-		blue = A[id + image_size];
-	}
-	if (colour_channel == 2) {
-		red = A[id - image_size - image_size];
-		green = A[id - image_size];
-		blue = A[id];
-	}
-	B[id] = red * 0.2126 + green * 0.7152 + blue * 0.0722;
-}
-
-//simple ND identity kernel
-kernel void identityND(global const uchar* A, global uchar* B) {
-	int width = get_global_size(0); //image width in pixels
-	int height = get_global_size(1); //image height in pixels
-	int image_size = width * height; //image size in pixels
-	int channels = get_global_size(2); //number of colour channels: 3 for RGB
-
-	int x = get_global_id(0); //current x coord.
-	int y = get_global_id(1); //current y coord.
-	int c = get_global_id(2); //current colour channel
-
-	int id = x + y * width + c * image_size; //global id in 1D space
-
-	B[id] = A[id];
-}
-
-//2D averaging filter
-kernel void avg_filterND(global const uchar* A, global uchar* B) {
-	int width = get_global_size(0); //image width in pixels
-	int height = get_global_size(1); //image height in pixels
-	int image_size = width * height; //image size in pixels
-	int channels = get_global_size(2); //number of colour channels: 3 for RGB
-
-	int x = get_global_id(0); //current x coord.
-	int y = get_global_id(1); //current y coord.
-	int c = get_global_id(2); //current colour channel
-
-	int id = x + y * width + c * image_size; //global id in 1D space
-
-	uint result = 0;
-
-	//simple boundary handling - just copy the original pixel
-	if ((x == 0) || (x == width - 2) || (y == 0) || (y == height - 2)) {
-		result = A[id];
-	}
-	else {
-		for (int i = (x - 2); i <= (x + 2); i++)
-			for (int j = (y - 2); j <= (y + 2); j++)
-				result += A[i + j * width + c * image_size];
-
-		result /= 25;
-	}
-
-	B[id] = (uchar)result;
-}
-
-//2D 3x3 convolution kernel
-kernel void convolutionND(global const uchar* A, global uchar* B, constant float* mask) {
-	int width = get_global_size(0); //image width in pixels
-	int height = get_global_size(1); //image height in pixels
-	int image_size = width * height; //image size in pixels
-	int channels = get_global_size(2); //number of colour channels: 3 for RGB
-
-	int x = get_global_id(0); //current x coord.
-	int y = get_global_id(1); //current y coord.
-	int c = get_global_id(2); //current colour channel
-
-	int id = x + y * width + c * image_size; //global id in 1D space
-
-	float result = 0;
-
-	//simple boundary handling - just copy the original pixel
-	if ((x == 0) || (x == width - 1) || (y == 0) || (y == height - 1)) {
-		result = A[id];
-	}
-	else {
-		for (int i = (x - 1); i <= (x + 1); i++)
-			for (int j = (y - 1); j <= (y + 1); j++)
-				result += A[i + j * width + c * image_size] * mask[i - (x - 1) + j - (y - 1)];
-	}
-
-	B[id] = (uchar)result;
-}
-kernel void intensityHistogram(global const uchar* A, global int* H) {
+﻿kernel void intensityHistogram(global const uchar* A, global int* H) {
 
 	int id = get_global_id(0);
 	int bin_index = A[id];
-	/*int temp = A[id];
-	printf("%d\n", temp);*/
+	//assumes bins are set to 0
 	atomic_inc(&H[bin_index]);//increments histogram bin
 
 }
+
+kernel void intensityHistogramLocal(global const uchar* A, local int* LH, global int* H, const int nr_bins) {
+	int id = get_global_id(0);
+	int lid = get_local_id(0);
+	int bin_index = A[id];
+	//clear the scratch bin
+	if (lid < nr_bins) {
+		LH[lid] = 0;
+	}
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	atomic_inc(&LH[bin_index]);
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	//copy LH from local to global
+	if (lid < nr_bins) {
+		atomic_add(&H[lid], LH[lid]);
+	}
+}
+
 kernel void cumulativeHistogram(global const int* H, global int* CH) {
 	int id = get_global_id(0);
 	
@@ -173,7 +39,7 @@ kernel void cumulativeHistogram(global const int* H, global int* CH) {
 kernel void NormaliseAndScale(global const int* CH, global int* NH, const float SF) {
 	int id = get_global_id(0);
 
-	NH[id] = CH[id] * SF;
+	NH[id] = CH[id] * SF;//multiplies each value by a scale factor which is input, which then normalises and scales the data within the range of either 0-255 or 0-65535
 }
 kernel void backProjection(global const uchar* A, global const int* NH, global uchar* B) {
 	int id = get_global_id(0);
